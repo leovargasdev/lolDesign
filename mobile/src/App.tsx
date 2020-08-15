1e5f0b1b887a9f4d3fffab5da7626ecac2c41f71
LeeonardoVargas <leu1607@hotmail.com>
2020-08-14 22:17:33 -0300

    Criando projeto mobile

---
+++ mobile/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <View>
+          <Text>aa</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
